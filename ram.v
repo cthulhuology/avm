c182d0949171fb99364561809174e80ac596cee9
Dave Goehrig <dave@dloh.org>
2016-11-04 20:49:53 -0400

    working on the ram layout

--- ram.v
+++ ram.v
@@ -1,1 +1,61 @@
-// TODO generate RAM
+// ram.v
+//
+// AVM ram block in Verilog 
+//
+// This is an implementation of AVM in C for experimenation purposes.
+// It is both an AVM interpretr and an assembler for the instruction set.
+//
+// (C) 2016 David J. Goehrig
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, 
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors 
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+
+`include "avm.vh"
+
+module ram (
+	input clock,
+	input we,
+	input inc,
+	input [`BITS-1:0] src_addr,
+	input [`BITS-1:0] dst_addr,
+	input [`BITS-1:0] data_in,
+	output [`BITS-1:0] data_out);
+
+	reg [`BITS-1:0] _src;
+	reg [`BITS-1:0] _dst;
+
+	always @(posedge clock) begin
+		if (we) begin
+			
+		end
+
+	end
+
+endmodule
+//
